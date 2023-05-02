8710cc3bd394b8136f4f410bea1a7c0d07b9864b
Mesh114 <117860547+Mesh114@users.noreply.github.com>
2023-05-02 17:06:37 +0000

    updating the code

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Check JS is Connected
-console.log('👍 JS Connected');
+const card = document.querySelector(".card__inner");
 
-// get all the card
-document.querySelectorAll('.card').forEach( item => {
-    
-    // for each item i.e. card, do this
-    item.addEventListener('click', event => {
-      
-      // toggle class on card
-      item.classList.toggle('flipped');
-    })
-  
-})
+card.addEventListener("click", function (e) {
+  card.classList.toggle('is-flipped');
+});
